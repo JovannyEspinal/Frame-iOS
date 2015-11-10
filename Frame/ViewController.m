1d1e3085bc77832ddbc9d0f583d6f6a72ddcb8ae
JovannyEspinal <espinaljovanny@icloud.com>
2015-11-10 12:41:49 -0500

    Tested out all necessary APIs

--- Frame/ViewController.m
+++ Frame/ViewController.m
@@ -9,7 +9,6 @@
 #import "ViewController.h"
 #import "LQNetworkManager.h"
 #import "AFNetworking.h"
-#import "APIManager.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *articleSnippet;
@@ -20,15 +19,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    AFHTTPRequestOperationManager *manager = [[AFHTTPRequestOperationManager alloc] init];
+   
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     
-    [manager GET:@"https://api.datumbox.com/1.0/SentimentAnalysis.json?api_key=8fe3f49401d945d0ca257445a6b1abef&text=i%20like%20cheese"
-       parameters:nil
-          success:^(AFHTTPRequestOperation *operation, id responseObject) {
-              NSLog(@"JSON: %@", responseObject);
-          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {l
-              NSLog(@"Error: %@", error);
-          }];
+    //Datumbox API
+//    [manager POST:@"http://api.datumbox.com/1.0/SubjectivityAnalysis.json"
+//       parameters:@{@"api_key": @"8fe3f49401d945d0ca257445a6b1abef",
+//                    @"text": self.articleSnippet.text}
+//          success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//              NSLog(@"JSON: %@", responseObject);
+//          } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//              NSLog(@"Error: %@", error);
+//          }];
+    
+    //Diffbot API
+    [manager GET:@"http://api.diffbot.com/v3/article"
+      parameters:@{@"token": @"fcbd17950b98e6d4138a9de24c642347",
+                   @"url": @"http://www.digitalspy.com/movies/star-wars/news/a773508/star-wars-fan-daniel-fleetwood-dies-aged-32/"}
+         success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+             
+             NSArray *data = responseObject[@"objects"];
+             NSString *text = [data firstObject][@"text"];
+             
+             NSLog(@"%@",text);
+         }
+         failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
+             NSLog(@"Failed.");
+         }];
     
     [[LQNetworkManager sharedManager] setApiKey:@"9b41f9a9dec46968939722b005f52be4"];
     
@@ -42,6 +59,8 @@
     // Dispose of any resources that can be recreated.
 }
 - (IBAction)politicalAnalysisTapped:(UIButton *)sender {
+    
+    //Indico API
     [[LQNetworkManager sharedManager] politicalAnalysis:self.articleSnippet.text completionHandler:^(NSDictionary *result, NSError *error) {
         if (!error) {
             NSLog(@"%@", result);
@@ -52,6 +71,7 @@
 }
 
 - (IBAction)sentimentAnalysisTapped:(UIButton *)sender {
+    //Indico API
     [[LQNetworkManager sharedManager] sentimentAnalysis:self.articleSnippet.text completionHandler:^(NSDictionary *result, NSError *error) {
         if (!error) {
             NSLog(@"%@", result);
